918a16201924be6c737fe8d344fb73692743c4f0
Jeremie Miller <jeremie@jabber.org>
2013-09-27 17:06:29 -0600

    generating an open packet, not valid yet tho

--- openize.c
+++ openize.c
@@ -11,14 +11,16 @@ int main(int argc, char *argv[])
 {
   ecc_key mykey, urkey;
   prng_state prng;
-  int err, x, ilen, seed_port;
-  unsigned char buf[4096], out[4096], str[1024], iv[16], key[32], inner[2048], *iat;
-  char inbuf[32768], *ibat, *pem, self_hn[64], seed_hn[64], *foo = "just testing", seed_ip[32];
+  int err, x, ilen, seed_port, inner_len, outer_len;
+  unsigned char buf[4096], out[4096], ecc_enc[256], rand16[16], aesiv[16], aeskey[32], inner[2048], inner_enc[2048], inner_sig[512], sighash[32], outer[2048];
+  char inbuf[32768], *ibat, *pem, self_hn[65], seed_hn[65], *foo = "just testing", seed_ip[32];
   char *pem_pre = "-----BEGIN PUBLIC KEY-----\n", *pem_suf = "\n-----END PUBLIC KEY-----\n";
-  unsigned long len;
-  unsigned short index[32];
+  char line_out[33], *iat, open[512], iv[33], sig[512];
+  unsigned char self_der[1024], self_eccpub[65];
+  unsigned long len, self_der_len;
+  unsigned short index[32], iatlen;
   symmetric_CTR ctr;
-  rsa_key rkey;
+  rsa_key self_rsa, seed_rsa;
   FILE *idfile;
 
   ltc_mp = ltm_desc;
@@ -57,29 +59,29 @@ int main(int argc, char *argv[])
   }
   
   // load the keypair
-  len = sizeof(buf);
-  if ((err = base64_decode(j0g_str("public",inbuf,index), strlen(j0g_str("public",inbuf,index)), buf, &len)) != CRYPT_OK) {
+  self_der_len = sizeof(self_der);
+  if ((err = base64_decode((unsigned char*)j0g_str("public",inbuf,index), strlen(j0g_str("public",inbuf,index)), self_der, &self_der_len)) != CRYPT_OK) {
     printf("base64 error: %s\n", error_to_string(err));
     return -1;
   }
-  if ((err = rsa_import(buf, len, &rkey)) != CRYPT_OK) {
+  if ((err = rsa_import(self_der, self_der_len, &self_rsa)) != CRYPT_OK) {
     printf("Import public error: %s\n", error_to_string(err));
     return -1;
   }
-  if ((err = hashname(buf, len, self_hn)) != CRYPT_OK) {
+  if ((err = hashname(self_der, self_der_len, self_hn)) != CRYPT_OK) {
     printf("hashname error: %s\n", error_to_string(err));
     return -1;
   }
   len = sizeof(buf);
-  if ((err = base64_decode(j0g_str("private",inbuf,index), strlen(j0g_str("private",inbuf,index)), buf, &len)) != CRYPT_OK) {
+  if ((err = base64_decode((unsigned char*)j0g_str("private",inbuf,index), strlen(j0g_str("private",inbuf,index)), buf, &len)) != CRYPT_OK) {
     printf("base64 error: %s\n", error_to_string(err));
     return -1;
   }
-  if ((err = rsa_import(buf, len, &rkey)) != CRYPT_OK) {
+  if ((err = rsa_import(buf, len, &self_rsa)) != CRYPT_OK) {
     printf("Import private error: %s\n", error_to_string(err));
     return -1;
   }  
-  printf("loaded keys for %.*s\n",64,self_hn);
+  printf("loaded keys for %s\n",self_hn);
 
   // load the seeds file
   idfile = fopen("seeds.json","rb");
@@ -119,11 +121,11 @@ int main(int argc, char *argv[])
   if(strstr(pem,pem_suf)) *(strstr(pem,pem_suf)) = 0;
   // pem should now be just the base64
   len = sizeof(buf);
-  if ((err = base64_decode(pem, strlen(pem), buf, &len)) != CRYPT_OK) {
+  if ((err = base64_decode((unsigned char*)pem, strlen(pem), buf, &len)) != CRYPT_OK) {
     printf("base64 error: %s\n", error_to_string(err));
     return -1;
   }
-  if ((err = rsa_import(buf, len, &rkey)) != CRYPT_OK) {
+  if ((err = rsa_import(buf, len, &seed_rsa)) != CRYPT_OK) {
     printf("Import public error: %s\n", error_to_string(err));
     return -1;
   }
@@ -133,86 +135,107 @@ int main(int argc, char *argv[])
   }
   strcpy(seed_ip,j0g_str("ip",ibat,index));
   seed_port = atoi(j0g_str("port",ibat,index));
-  printf("seed loaded %.*s at %s:%d\n",64,seed_hn,seed_ip,seed_port);
-  
+  printf("seed loaded %s at %s:%d\n",seed_hn,seed_ip,seed_port);
 
-  iat = inner+2;
-//  sprintf(iat, "{\"line\":\"%s\",\"at\":%d,\"to\":\"%s\"}",);
+  /////////////////
+  // create the inner packet
 
+  // random line id
   if ((err = rng_make_prng(128, find_prng("yarrow"), &prng, NULL)) != CRYPT_OK) {
     printf("Error setting up PRNG, %s\n", error_to_string(err)); return -1;
   }
+  x = yarrow_read(rand16,16,&prng);
+  if (x != 16) {
+    printf("Error reading PRNG for line required.\n");
+    return -1;
+  }
+  hex(rand16,16,line_out);
 
-  // ECC TESTING
+  iat = (char*)inner+2;
+  sprintf(iat, "{\"line\":\"%s\",\"at\":%d,\"to\":\"%s\"}",line_out,(int)time(0),seed_hn);
+  printf("INNER: %s\n",iat);
+  iatlen = htons(strlen(iat));
+  memcpy(inner,&iatlen,2);
+  inner_len = 2+strlen(iat);
+  memcpy(inner+inner_len,self_der,self_der_len);
+  inner_len += self_der_len;
+  printf("inner packet len %d\n",inner_len);
+
+
+  // generate the ecc keys
   if ((err = ecc_make_key(&prng, find_prng("yarrow"), 32, &mykey)) != CRYPT_OK) {
     printf("Error making key: %s\n", error_to_string(err)); return -1;
   }
-  
-  mp_to_unsigned_bin(mykey.pubkey.x, str);
-  x = mp_unsigned_bin_size(mykey.pubkey.y);  
-  printf("ecc key x %d X %s\n",x,hex(str,x,out));
+  *self_eccpub = 0x04;
+  mp_to_unsigned_bin(mykey.pubkey.x, self_eccpub+1);
+  mp_to_unsigned_bin(mykey.pubkey.y, self_eccpub+33);
+  printf("ecc pub key %s\n",hex(self_eccpub,65,(char*)out));
 
-  if ((err = ecc_make_key(&prng, find_prng("yarrow"), 32, &urkey)) != CRYPT_OK) {
-    printf("Error making key: %s\n", error_to_string(err)); return -1;
-  }
-
-  x = sizeof(str);
-  if ((err = ecc_shared_secret(&mykey,&urkey,str,&x)) != CRYPT_OK) {
-    printf("Error making key: %s\n", error_to_string(err)); return -1;
-  }
-  printf("shared secret %d %s\n",x,hex(str,x,out));
-  
-
-
-  // SHA256 TESTING
+  // create the aes cipher key/iv
   len = 32;
-  if ((err = hash_memory(find_hash("sha256"),foo,strlen(foo),key,&len)) != CRYPT_OK) {
+  if ((err = hash_memory(find_hash("sha256"),self_eccpub,65,aeskey,&len)) != CRYPT_OK) {
      printf("Error hashing key: %s\n", error_to_string(err));
      return -1;
   }
-  printf("sha256 %s\n",hex(key,32,out));
-
-
-  // AES TESTING
-  x = yarrow_read(iv,16,&prng);
+  x = yarrow_read(aesiv,16,&prng);
   if (x != 16) {
     printf("Error reading PRNG for IV required.\n");
     return -1;
   }
-  printf("random IV %s\n",hex(iv,16,out));
+  hex(aesiv,16,iv);
 
-  if ((err = ctr_start(find_cipher("aes"),iv,key,32,0,CTR_COUNTER_LITTLE_ENDIAN,&ctr)) != CRYPT_OK) {
+  // create aes cipher now and encrypt the inner
+  if ((err = ctr_start(find_cipher("aes"),aesiv,aeskey,32,0,CTR_COUNTER_LITTLE_ENDIAN,&ctr)) != CRYPT_OK) {
      printf("ctr_start error: %s\n",error_to_string(err));
      return -1;
   }
-
-  if ((err = ctr_encrypt(foo,str,strlen(foo),&ctr)) != CRYPT_OK) {
+  if ((err = ctr_encrypt(inner,inner_enc,inner_len,&ctr)) != CRYPT_OK) {
      printf("ctr_encrypt error: %s\n", error_to_string(err));
      return -1;
   }
-  printf("aes encrypt %s\n",hex(str,strlen(foo),out));
+  printf("aes encrypt %s\n",hex(inner_enc,inner_len,out));
 
-
-  // RSA TESTING
-  if ((err = rsa_make_key(&prng, find_prng("yarrow"), 256, 65537, &rkey)) != CRYPT_OK) {
-    printf("rsa_make_key error: %s\n", error_to_string(err));
-    return -1;
-  }
-
-  x = sizeof(str);
-  if ((err = rsa_export(str, &x, PK_PUBLIC, &rkey)) != CRYPT_OK) {
-    printf("Export error: %s\n", error_to_string(err));
-    return -1;
-  }
-  printf("rsa key size %d key %s\n",x,hex(str,x,out));
-
-  x = sizeof(str);
-  if ((err = rsa_encrypt_key(foo, strlen(foo), str, &x, "TestApp", 7, &prng, find_prng("yarrow"), find_hash("sha1"), &rkey)) != CRYPT_OK) {
+  // encrypt the ecc key
+  x = sizeof(ecc_enc);
+  if ((err = rsa_encrypt_key(self_eccpub, 65, ecc_enc, &x, 0, 0, &prng, find_prng("yarrow"), find_hash("sha1"), &seed_rsa)) != CRYPT_OK) {
     printf("rsa_encrypt error: %s\n", error_to_string(err));
     return -1;
   }
-  printf("rsa enc size %d of %s\n",x,hex(str,x,out));
+  len = sizeof(open);
+  if ((err = base64_encode(ecc_enc, x, open, &len)) != CRYPT_OK) {
+    printf("base64 error: %s\n", error_to_string(err));
+    return -1;
+  }
+  open[len] = 0;
 
+  // sign the inner_enc
+  len = 32;
+  if ((err = hash_memory(find_hash("sha256"),inner_enc,inner_len,sighash,&len)) != CRYPT_OK) {
+     printf("Error hashing key: %s\n", error_to_string(err));
+     return -1;
+  }
+  x = sizeof(inner_sig);
+  if ((err = rsa_sign_hash_ex(sighash, 32, inner_sig, &x, LTC_PKCS_1_V1_5, &prng, find_prng("yarrow"), find_hash("sha256"), 12, &self_rsa)) != CRYPT_OK) {
+    printf("rsa_sign error: %s\n", error_to_string(err));
+    return -1;
+  }
+  len = sizeof(sig);
+  if ((err = base64_encode(inner_sig, x, sig, &len)) != CRYPT_OK) {
+    printf("base64 error: %s\n", error_to_string(err));
+    return -1;
+  }
+  sig[len] = 0;
+  
+  // create the outer open packet
+  iat = (char*)outer+2;
+  sprintf(iat, "{\"type\":\"open\",\"open\":\"%s\",\"iv\":\"%s\",\"sig\":\"%s\"}",open,iv,sig);
+  printf("OUTER: %s\n",iat);
+  iatlen = htons(strlen(iat));
+  memcpy(outer,&iatlen,2);
+  outer_len = 2+strlen(iat);
+  memcpy(outer+outer_len,inner_enc,inner_len);
+  outer_len += inner_len;
+  printf("outer packet len %d: %s\n",outer_len,hex(outer,outer_len,out));  
 
   return 0;
 }
