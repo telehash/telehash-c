5ad7f64eb493117f952cb314e3553a94e1d3bebd
Jeremie Miller <jeremie@jabber.org>
2014-04-12 05:10:46 -0600

    whitespace cleanup

--- lib/util.c
+++ lib/util.c
@@ -26,20 +26,20 @@ unsigned char hexcode(unsigned char x)
     else if (x >= 'a' && x <= 'f')    /* a-f offset by hex 37 */
       return(x - 0x57);
     else {                            /* Otherwise, an illegal hex digit */
-		return x;
+      return x;
     }
 }
 
 unsigned char *util_unhex(unsigned char *in, int len, unsigned char *out)
 {
-	int j;
-	unsigned char *c = out;
-	for(j=0; (j+1)<len; j+=2)
-	{
-		*c = ((hexcode(in[j]) * 16) & 0xF0) + (hexcode(in[j+1]) & 0xF);
-		c++;
-	}
-	return out;
+  int j;
+  unsigned char *c = out;
+  for(j=0; (j+1)<len; j+=2)
+  {
+    *c = ((hexcode(in[j]) * 16) & 0xF0) + (hexcode(in[j+1]) & 0xF);
+    c++;
+  }
+  return out;
 }
 
 int util_cmp(char *a, char *b)
