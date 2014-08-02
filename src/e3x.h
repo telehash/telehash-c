83b9f6ef3eb23f768b295eb3d327f45209958eb9
Jeremie Miller <jeremie@jabber.org>
2014-08-01 23:07:00 -0600

    reorg the header spec to make the e3x exchange the primary interaction

--- src/e3x.h
+++ src/e3x.h
@@ -2,56 +2,62 @@
 #define e3x_h
 
 // this includes generic buffer encoding/decoding to packets
-// also defines packet_t that has a ->eid, ->cid, ->arg
+// also defines packet_t that has a ->token, ->arg
 #include "packet.h"
 
-typedef struct e3x_struct
-{
-  uint8_t id[32];
-  xht_t sids, eids;
-} *e3x_t;
+// hn_t
+#include "hashname.h"
 
-// pass in a prime to optimize the eid lookup table, 0 to use default
-e3x_t e3x_new(uint32_t prime, packet_t keys);
 
-// will immediately free everything
-e3x_t e3x_free(e3x_t);
+// my id and crypto keys
+typedef struct e3x_self_struct *e3x_self_t;
+e3x_self_t e3x_self_new(packet_t keys);
+void e3x_self_free(e3x_self_t);
 
-// parts must contain body of the key too
-uint8_t *e3x_endpoint(packet_t parts);
+// a single exchange
+typedef struct e3x_struct *e3x_t;
+
+// to must contain keys or a handshake
+// make a new exchange
+e3x_t e3x_new(e3x_self_t self, packet_t to);
 
 // will error any active channels and then free any caches after
-void e3x_drop(uint8_t *eid);
+void e3x_free(e3x_t e);
 
-packet_t e3x_decrypt(uint8_t *eid, uint8_t *buffer, uint32_t len);
-uint8_t *e3x_encrypt(uint8_t *eid, packet_t packet, uint8_t *len);
+// get the 16 byte unique id
+uint8_t *e3x_token(e3x_t e);
+
+// simple encrypt/decrypt conversion of a packet
+packet_t e3x_decrypt(e3x_t e, packet_t packet);
+packet_t e3x_encrypt(e3x_t e, packet_t packet);
 
 // processes data coming from any network interface
-packet_t e3x_process(uint8_t *buffer, uint8_t len);
+// packet has either ->token or contains a handshake
+packet_t e3x_self_receive(e3x_self_t self, packet_t packet);
 
-// only pass in packets returned from _process, if their ->eid is trusted
+// only pass in packets returned from _receive, only handshakes that are trusted
 // returns 0 for dropped packets, 1 for new/valid ones
-uint8_t e3x_receive(packet_t packet);
+uint8_t e3x_receive(e3x_t e, packet_t packet);
 
-// use when the network transport needs to be re-validated for an eid
+// use when the network transport needs to be re-validated for an exchange
 // generates a handshake if there's any channels, will re-send and start to timeout channels if not confirmed
-uint8_t e3x_keepalive(uint8_t *eid, uint8_t *len);
+packet_t e3x_keepalive(e3x_t e);
 
-// returns the next eid with any packets to send/receive
-uint8_t *e3x_busy();
+// returns the next one with any packets to send/receive
+uint32_t e3x_next(e3x_t e);
 
-// get the next buffer to send to this eid (if any), caller free's
-uint8_t *e3x_sending(uint8_t *eid, uint8_t *len);
+// get the next packet to send to them (if any), caller free's
+packet_t e3x_sending(e3x_t e);
 
-// get the next packet received from this eid, will have ->cid for the channel
-packet_t e3x_receiving(uint8_t *eid);
+// get the next packet received from them, will have ->cid for the channel
+packet_t e3x_receiving(e3x_t e);
 
 // create new reliable (1) or unreliable (2) channel id, timeout is for start/acks/ends
-uint32_t e3x_channel(uint8_t *eid, uint8_t kind, uint32_t timeout);
+uint32_t e3x_channel(e3x_t e, uint8_t kind, uint32_t timeout);
 
 // create a new packet to send to this channel
 // packets created must be given to send() in order
-packet_t e3x_packet(uint8_t *eid, uint32_t cid);
+packet_t e3x_packet(e3x_t e, uint32_t cid);
 
 // encrypts and shows up in sending(), queues and triggers handshake too
 // 0 if not delivered, else is # of packets waiting to be sent and/or ack'd
