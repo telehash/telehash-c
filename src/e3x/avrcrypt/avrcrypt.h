ff60969238b389f2c4715651cc8d6e6ad9dff7ff
Jeremie Miller <jeremie@jabber.org>
2014-11-05 05:40:30 -0700

    start moving arduino/avr into it's own area

--- src/e3x/avrcrypt/avrcrypt.h
+++ src/e3x/avrcrypt/avrcrypt.h
@@ -2,12 +2,10 @@
 #define avrcrypt_h
 
 #include "platform.h"
-#include <switch.h>
+#include <mesh.h>
 #include "uECC.h"
 #include "sha256.h"
 #include "hmac.h"
-#include "base64_enc.h"
-#include "base64_dec.h"
 #include "aes.h"
 
 #endif
