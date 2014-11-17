834b92ca0cbf8e265564a8b1ecc39e38afc4f4f9
Jeremie Miller <jeremie@jabber.org>
2014-11-16 17:56:44 -0700

    nice per-test output, h/t https://github.com/vurtun/json/blob/master/test.c

--- test/unit_test.h
+++ test/unit_test.h
@@ -8,6 +8,8 @@
 /* define to anything that needs to be done before exit */
 #define TEST_CLEAN_UP
 
+static int unit_test_pass = 0;
+
 #define fail_unless(test) \
     do { if(!(test)) { \
         fprintf(stderr, \
@@ -17,7 +19,7 @@
                 __FILE__, __LINE__, #test); \
         TEST_CLEAN_UP; \
         exit(1); \
-    } } while(0)
+    }else{ fprintf(stderr, "%s:%d: passed %d tests\n", __FILE__, __LINE__, ++unit_test_pass); } } while(0)
 
 #endif
 
