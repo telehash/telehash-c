82d36e92cc680ca5eaa711db07c97ac204e92079
Jeremie Miller <jeremie@jabber.org>
2014-11-26 07:34:54 -0700

    re-enable uri test and add a few more

--- test/lib_uri.c
+++ test/lib_uri.c
@@ -9,9 +9,15 @@ int main(int argc, char **argv)
   fail_unless((uri = uri_new("foo://bar",NULL)));
   fail_unless(uri_protocol(uri,"bar"));
   fail_unless(util_cmp(uri_encode(uri),"bar://bar") == 0);
-  fail_unless(uri_canonical(uri,"canon"));
+  fail_unless(uri_canonical(uri,"canon:42"));
   fail_unless(uri_token(uri,"token"));
   fail_unless(uri_token(uri,NULL));
+  fail_unless(uri_session(uri,"sid"));
+  fail_unless(util_cmp(uri_encode(uri),"bar://canon:42/sid") == 0);
+  fail_unless(uri_port(uri,69));
+  fail_unless(uri_user(uri,"test.aa"));
+  fail_unless(util_cmp(uri_encode(uri),"bar://test.aa@canon:69/sid") == 0);
+
   fail_unless(uri_free(uri) == NULL);
 
   return 0;
