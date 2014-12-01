a950ec64370547402db49864d3de90ef84730f9c
Jeremie Miller <jeremie@jabber.org>
2014-12-01 10:49:16 -0700

    fix tests for timer changes

--- test/e3x_event3.c
+++ test/e3x_event3.c
@@ -1,14 +1,16 @@
 #include "e3x.h"
 #include "util.h"
+#include "platform.h"
 #include "unit_test.h"
 
 int main(int argc, char **argv)
 {
   fail_unless(e3x_init(NULL) == 0);
 
-  event3_t ev = event3_new(3);
+  uint64_t now = platform_ms(0);
+  event3_t ev = event3_new(3, now);
   fail_unless(ev);
-  fail_unless(event3_at(ev) == 0);
+  fail_unless(event3_at(ev, now));
 
   // TODO test the rest!
 
