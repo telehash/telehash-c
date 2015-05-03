c3c19c8550326532d38a35984ba97cb9298cdac3
Jeremie Miller <jeremie@jabber.org>
2015-05-03 13:12:03 -0600

    stub router request

--- include/ext_peer.h
+++ include/ext_peer.h
@@ -3,6 +3,9 @@
 
 #include "ext.h"
 
+// try to connect this peer via this router (sends a peer request)
+link_t peer_router(link_t peer, link_t router);
+
 // handle incoming peer request to route
 lob_t peer_on_open(link_t link, lob_t open);
 
--- src/ext/peer.c
+++ src/ext/peer.c
@@ -1,5 +1,13 @@
 #include "ext.h"
 
+link_t peer_router(link_t peer, link_t router)
+{
+  if(!peer || !router) return LOG("bad args");
+
+  // TODO compose best open for peer
+  return NULL;
+}
+
 // handle incoming peer request to route
 lob_t peer_on_open(link_t link, lob_t open)
 {
@@ -10,7 +18,11 @@ lob_t peer_on_open(link_t link, lob_t open)
   LOG("incoming peer route request %s",lob_json(open));
 
   peer = xht_get(link->mesh->index,lob_get(open,"peer"));
-  if(!peer) return LOG("no peer link found");
+  if(!peer)
+  {
+    LOG("no peer link found");
+    return open;
+  }
   
   lob_set(open,"type","connect");
   link_send(peer,open);
