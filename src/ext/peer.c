b09de424d82dcffcba51edcd905ac26e7d215a7f
Jeremie Miller <jeremie@jabber.org>
2015-05-03 10:48:34 -0600

    start forwarding peer packets

--- src/ext/peer.c
+++ src/ext/peer.c
@@ -3,15 +3,17 @@
 // handle incoming peer request to route
 lob_t peer_on_open(link_t link, lob_t open)
 {
+  link_t peer;
   if(!link) return open;
   if(lob_get_cmp(open,"type","peer")) return open;
   
   LOG("incoming peer route request %s",lob_json(open));
 
-  // TODO
-  // find/verify destination link
-  // generate connect request to them
+  peer = xht_get(link->mesh->index,lob_get(open,"peer"));
+  if(!peer) return LOG("no peer link found");
   
+  lob_set(open,"type","connect");
+  link_send(peer,open);
 
-  return NULL;
+  return LOG("connect generated to %s",peer->id->hashname);
 }
