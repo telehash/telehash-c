1af5d5ee829ed3e8ff2959a5e91cd83f1a96fbc9
Jeremie Miller <jeremie@jabber.org>
2015-08-02 07:28:54 -0600

    refactoring the epoch/medium definitons based on current tmesh draft

--- include/epoch.h
+++ include/epoch.h
@@ -3,48 +3,43 @@
 #include <stdint.h>
 
 typedef struct epoch_struct *epoch_t;
+typedef struct medium_struct *medium_t;
 typedef struct knock_struct *knock_t;
 
 #include "mesh.h"
 #include "link.h"
 
-struct epoch_struct
-{
-  uint8_t bin[16]; // 8 header 8 random body
-  uint8_t iv[8]; // base IV for MAC-AES
-  uint64_t bday; // microsecond of window 0 start
-  epoch_t next; // for epochs_* interface, lists
-  uint32_t busy; // microseconds to tx/rx, set by external/phy
-  uint8_t chans; // number of total channels, set by external/phy
-};
-
+// knock state holder when sending/receiving
 struct knock_struct
 {
-  epoch_t e;
   uint32_t win; // current window id
-  uint32_t chan; // current channel (< e->chans)
+  uint32_t chan; // current channel (< med->chans)
   uint64_t start, stop; // microsecond exact start/stop time
   uint8_t *buf; // filled in by scheduler (tx) or driver (rx)
-  knock_t next;
-  uint8_t tx; // boolean if is a tx or rx
   uint8_t len; // <= 64
 };
 
-epoch_t epoch_new(char *id);
+// individual epoch+medium state data, goal to keep <64b each
+struct epoch_struct
+{
+  uint64_t base; // microsecond of window 0 start
+  uint8_t secret[32];
+  knock_t active; // only when scheduled
+  epoch_t next; // for epochs_* list utils
+  void *phy; // used by medium PHY driver
+  uint32_t busy; // microseconds to tx/rx, set by PHY driver
+  uint8_t chans; // number of total channels, set by PHY driver
+  uint8_t type; // medium type
+  uint8_t tx; // boolean if is a tx or rx
+};
+
+
+epoch_t epoch_new(uint8_t type, uint8_t tx);
 epoch_t epoch_free(epoch_t e);
-epoch_t epoch_reset(epoch_t e);
-char *epoch_id(epoch_t e); // friendly base32 encoding
-epoch_t epoch_import(epoch_t e, char *id, char *body); // also resets, body optional
+epoch_t epoch_base(epoch_t e, uint32_t window, uint64_t at); // sync point for given window
 
-// scheduling stuff
-epoch_t epoch_sync(epoch_t e, uint32_t window, uint64_t at); // sync point for given window
-knock_t knock_new(uint8_t tx); // generate a new/blank knock
-knock_t epoch_knock(epoch_t e, knock_t k, uint64_t from); // init knock to current window of from
-knock_t knock_free(knock_t k); // frees
-
-// phy utilities
-epoch_t epoch_busy(epoch_t e, uint32_t us); // microseconds for how long the action takes
-epoch_t epoch_chans(epoch_t e, uint8_t chans); // number of possible channels
+// reset knock to next window, 0 cleans out
+epoch_t epoch_knock(epoch_t e, uint64_t at);
 
 // simple array utilities
 epoch_t epochs_add(epoch_t es, epoch_t e);
--- include/net_tmesh.h
+++ include/net_tmesh.h
@@ -11,9 +11,9 @@ typedef struct mote_struct
 {
   tmesh_t tm;
   util_chunks_t chunks;
-  epoch_t active;
-  epoch_t syncs;
-  knock_t tx, rx; // only one of each ever exists, is re-set each loop
+  epoch_t epochs;
+  epoch_t syncs; // only when synchronizing
+  epoch_t tx, rx; // currently active ones only
   link_t link;
   uint8_t z;
   pipe_t pipe;
@@ -27,22 +27,22 @@ struct tmesh_struct
   mesh_t mesh;
   mote_t motes;
   lob_t path;
-  mote_t disco; // virtual mote, only when discovery is enabled
+  epoch_t disco; // only when discovery is enabled
   epoch_t syncs; // the ones we listen on
-  knock_t tx, rx; // soft scheduled
-  knock_t active; // hard scheduled
-  epoch_t (*init)(tmesh_t tm, epoch_t e); // callback used to initialize all new epochs, add scheduling time/cost and channels
+  epoch_t tx, rx; // soft scheduled
+  epoch_t active; // hard scheduled
+  epoch_t (*medium)(tmesh_t tm, epoch_t e, char *medium); // callback used to initialize all new epochs, add medium scheduling time/cost and channels
 };
 
 // create a new tmesh radio network bound to this mesh
 tmesh_t tmesh_new(mesh_t mesh, lob_t options, epoch_t (*init)(tmesh_t tm, epoch_t e));
 void tmesh_free(tmesh_t tm);
 
-// add a sync epoch from this header, right now must be called at initialization/creation only
-tmesh_t tmesh_sync(tmesh_t tm, char *header);
+// add a sync epoch from this medium, must be called at initialization/creation only
+tmesh_t tmesh_sync(tmesh_t tm, char *medium);
 
-// become discoverable by anyone with this epoch id, pass NULL to reset all
-tmesh_t tmesh_discoverable(tmesh_t tm, char *id);
+// become discoverable by anyone with this medium and network, pass NULL to reset all
+tmesh_t tmesh_discoverable(tmesh_t tm, char *medium, char *network);
 
 // get(create) the mote for a link
 mote_t tmesh_mote(tmesh_t tm, link_t link);
