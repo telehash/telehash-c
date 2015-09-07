f1829f43622081df493a66c70b45914064e44fb8
Jeremie Miller <jeremie@jabber.org>
2015-09-06 18:01:26 -0600

    static make builds now generate full lib header files too

---
+++ src/e3x/cs1a/cs1a.h
@@ -0,0 +1,18 @@
+#ifndef cs1a_h
+#define cs1a_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "telehash.h"
+#include "sha256.h"
+#include "uECC.h"
+#include "aes.h"
+#include "hmac.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
