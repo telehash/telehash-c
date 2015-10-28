032ebad8f0c526aeca57229e4d6df5d00d492c46
Jeremie Miller <jeremie@jabber.org>
2015-10-28 16:05:10 -0600

    replace chacha implementation w/ reference one

--- src/lib/chacha.c
+++ src/lib/chacha.c
@@ -1,136 +1,254 @@
 #include "telehash.h"
 
-// public domain, original source: https://gist.github.com/thoughtpolice/2b36e168d2d7582ad58b
+/*
+ chacha-merged.c version 20080118
+ D. J. Bernstein
+ Public domain.
+ */
 
-static unsigned int
-L32(unsigned int x,unsigned int n)
+#include <sys/types.h>
+#include <stddef.h>
+
+struct chacha_ctx {
+	unsigned input[16];
+};
+
+#define CHACHA_MINKEYLEN 	16
+#define CHACHA_NONCELEN		8
+#define CHACHA_CTRLEN		8
+#define CHACHA_STATELEN		(CHACHA_NONCELEN+CHACHA_CTRLEN)
+#define CHACHA_BLOCKLEN		64
+
+void chacha_keysetup(struct chacha_ctx *x, const u_char *k, u_int kbits)
+    __attribute__((__bounded__(__minbytes__, 2, CHACHA_MINKEYLEN)));
+void chacha_ivsetup(struct chacha_ctx *x, const u_char *iv, const u_char *ctr)
+    __attribute__((__bounded__(__minbytes__, 2, CHACHA_NONCELEN)))
+    __attribute__((__bounded__(__minbytes__, 3, CHACHA_CTRLEN)));
+void chacha_encrypt_bytes(struct chacha_ctx *x, const u_char *m,
+    u_char *c, u_int bytes)
+    __attribute__((__bounded__(__buffer__, 2, 4)))
+    __attribute__((__bounded__(__buffer__, 3, 4)));
+
+/* $OpenBSD: chacha.c,v 1.1 2013/11/21 00:45:44 djm Exp $ */
+
+typedef unsigned char u8;
+typedef unsigned int u32;
+
+typedef struct chacha_ctx chacha_ctx;
+
+#define U8C(v) (v##U)
+#define U32C(v) (v##U)
+
+#define U8V(v) ((u8)(v) & U8C(0xFF))
+#define U32V(v) ((u32)(v) & U32C(0xFFFFFFFF))
+
+#define ROTL32(v, n) \
+  (U32V((v) << (n)) | ((v) >> (32 - (n))))
+
+#define U8TO32_LITTLE(p) \
+  (((u32)((p)[0])      ) | \
+   ((u32)((p)[1]) <<  8) | \
+   ((u32)((p)[2]) << 16) | \
+   ((u32)((p)[3]) << 24))
+
+#define U32TO8_LITTLE(p, v) \
+  do { \
+    (p)[0] = U8V((v)      ); \
+    (p)[1] = U8V((v) >>  8); \
+    (p)[2] = U8V((v) >> 16); \
+    (p)[3] = U8V((v) >> 24); \
+  } while (0)
+
+#define ROTATE(v,c) (ROTL32(v,c))
+#define XOR(v,w) ((v) ^ (w))
+#define PLUS(v,w) (U32V((v) + (w)))
+#define PLUSONE(v) (PLUS((v),1))
+
+#define QUARTERROUND(a,b,c,d) \
+  a = PLUS(a,b); d = ROTATE(XOR(d,a),16); \
+  c = PLUS(c,d); b = ROTATE(XOR(b,c),12); \
+  a = PLUS(a,b); d = ROTATE(XOR(d,a), 8); \
+  c = PLUS(c,d); b = ROTATE(XOR(b,c), 7);
+
+static const char sigma[16] = "expand 32-byte k";
+static const char tau[16] = "expand 16-byte k";
+
+void chacha_keysetup (chacha_ctx *x, const u8 *k, u32 kbits)
 {
-  /* See: http://blog.regehr.org/archives/1063 */
-  return (x<<n) | (x>>(-n&31));
+	const char *constants;
+
+	x->input[4] = U8TO32_LITTLE(k + 0);
+	x->input[5] = U8TO32_LITTLE(k + 4);
+	x->input[6] = U8TO32_LITTLE(k + 8);
+	x->input[7] = U8TO32_LITTLE(k + 12);
+	if (kbits == 256) { /* recommended */
+		k += 16;
+		constants = sigma;
+	}
+	else { /* kbits == 128 */
+		constants = tau;
+	}
+	x->input[8] = U8TO32_LITTLE(k + 0);
+	x->input[9] = U8TO32_LITTLE(k + 4);
+	x->input[10] = U8TO32_LITTLE(k + 8);
+	x->input[11] = U8TO32_LITTLE(k + 12);
+	x->input[0] = U8TO32_LITTLE(constants + 0);
+	x->input[1] = U8TO32_LITTLE(constants + 4);
+	x->input[2] = U8TO32_LITTLE(constants + 8);
+	x->input[3] = U8TO32_LITTLE(constants + 12);
 }
 
-static unsigned int
-ld32(const unsigned char* x)
+void chacha_ivsetup (chacha_ctx *x, const u8 *iv, const u8 *counter)
 {
-  unsigned int u = x[3];
-  u = (u<<8)|x[2];
-  u = (u<<8)|x[1];
-  return (u<<8)|x[0];
+	x->input[12] = counter == NULL ? 0 : U8TO32_LITTLE(counter + 0);
+	x->input[13] = counter == NULL ? 0 : U8TO32_LITTLE(counter + 4);
+	x->input[14] = U8TO32_LITTLE(iv + 0);
+	x->input[15] = U8TO32_LITTLE(iv + 4);
 }
 
-static void st32(unsigned char *x,unsigned int u)
+void chacha_encrypt_bytes (chacha_ctx *x, const u8 *m, u8 *c, u32 bytes)
 {
-  int i;
-  for (i=0; i < 4; ++i) { x[i] = (0xff & u); u >>= 8; }
+	u32 x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15;
+	u32 j0, j1, j2, j3, j4, j5, j6, j7, j8, j9, j10, j11, j12, j13, j14, j15;
+	u8 *ctarget = NULL;
+	u8 tmp[64];
+	u_int i;
+
+	if (!bytes)
+		return;
+
+	j0 = x->input[0];
+	j1 = x->input[1];
+	j2 = x->input[2];
+	j3 = x->input[3];
+	j4 = x->input[4];
+	j5 = x->input[5];
+	j6 = x->input[6];
+	j7 = x->input[7];
+	j8 = x->input[8];
+	j9 = x->input[9];
+	j10 = x->input[10];
+	j11 = x->input[11];
+	j12 = x->input[12];
+	j13 = x->input[13];
+	j14 = x->input[14];
+	j15 = x->input[15];
+
+	for (;;) {
+		if (bytes < 64) {
+			for (i = 0; i < bytes; ++i)
+				tmp[i] = m[i];
+			m = tmp;
+			ctarget = c;
+			c = tmp;
+		}
+		x0 = j0;
+		x1 = j1;
+		x2 = j2;
+		x3 = j3;
+		x4 = j4;
+		x5 = j5;
+		x6 = j6;
+		x7 = j7;
+		x8 = j8;
+		x9 = j9;
+		x10 = j10;
+		x11 = j11;
+		x12 = j12;
+		x13 = j13;
+		x14 = j14;
+		x15 = j15;
+		for (i = 20; i > 0; i -= 2) {
+			QUARTERROUND(x0, x4, x8, x12)
+			QUARTERROUND(x1, x5, x9, x13)
+			QUARTERROUND(x2, x6, x10, x14)
+			QUARTERROUND(x3, x7, x11, x15)
+			QUARTERROUND(x0, x5, x10, x15)
+			QUARTERROUND(x1, x6, x11, x12)
+			QUARTERROUND(x2, x7, x8, x13)
+			QUARTERROUND(x3, x4, x9, x14)
+		}
+		x0 = PLUS(x0, j0);
+		x1 = PLUS(x1, j1);
+		x2 = PLUS(x2, j2);
+		x3 = PLUS(x3, j3);
+		x4 = PLUS(x4, j4);
+		x5 = PLUS(x5, j5);
+		x6 = PLUS(x6, j6);
+		x7 = PLUS(x7, j7);
+		x8 = PLUS(x8, j8);
+		x9 = PLUS(x9, j9);
+		x10 = PLUS(x10, j10);
+		x11 = PLUS(x11, j11);
+		x12 = PLUS(x12, j12);
+		x13 = PLUS(x13, j13);
+		x14 = PLUS(x14, j14);
+		x15 = PLUS(x15, j15);
+
+		x0 = XOR(x0, U8TO32_LITTLE(m + 0));
+		x1 = XOR(x1, U8TO32_LITTLE(m + 4));
+		x2 = XOR(x2, U8TO32_LITTLE(m + 8));
+		x3 = XOR(x3, U8TO32_LITTLE(m + 12));
+		x4 = XOR(x4, U8TO32_LITTLE(m + 16));
+		x5 = XOR(x5, U8TO32_LITTLE(m + 20));
+		x6 = XOR(x6, U8TO32_LITTLE(m + 24));
+		x7 = XOR(x7, U8TO32_LITTLE(m + 28));
+		x8 = XOR(x8, U8TO32_LITTLE(m + 32));
+		x9 = XOR(x9, U8TO32_LITTLE(m + 36));
+		x10 = XOR(x10, U8TO32_LITTLE(m + 40));
+		x11 = XOR(x11, U8TO32_LITTLE(m + 44));
+		x12 = XOR(x12, U8TO32_LITTLE(m + 48));
+		x13 = XOR(x13, U8TO32_LITTLE(m + 52));
+		x14 = XOR(x14, U8TO32_LITTLE(m + 56));
+		x15 = XOR(x15, U8TO32_LITTLE(m + 60));
+
+		j12 = PLUSONE(j12);
+		if (!j12) {
+			j13 = PLUSONE(j13);
+			/* stopping at 2^70 bytes per nonce is user's responsibility */
+		}
+
+		U32TO8_LITTLE(c + 0, x0);
+		U32TO8_LITTLE(c + 4, x1);
+		U32TO8_LITTLE(c + 8, x2);
+		U32TO8_LITTLE(c + 12, x3);
+		U32TO8_LITTLE(c + 16, x4);
+		U32TO8_LITTLE(c + 20, x5);
+		U32TO8_LITTLE(c + 24, x6);
+		U32TO8_LITTLE(c + 28, x7);
+		U32TO8_LITTLE(c + 32, x8);
+		U32TO8_LITTLE(c + 36, x9);
+		U32TO8_LITTLE(c + 40, x10);
+		U32TO8_LITTLE(c + 44, x11);
+		U32TO8_LITTLE(c + 48, x12);
+		U32TO8_LITTLE(c + 52, x13);
+		U32TO8_LITTLE(c + 56, x14);
+		U32TO8_LITTLE(c + 60, x15);
+
+		if (bytes <= 64) {
+			if (bytes < 64) {
+				for (i = 0; i < bytes; ++i)
+					ctarget[i] = c[i];
+			}
+			x->input[12] = j12;
+			x->input[13] = j13;
+			return;
+		}
+		bytes -= 64;
+		c += 64;
+		m += 64;
+	}
 }
 
-static void
-QR(unsigned int* x,
-   unsigned int a,
-   unsigned int b,
-   unsigned int c,
-   unsigned int d)
-{
-  x[a] = x[a]+x[b]; x[d] = L32(x[d]^x[a],16);
-  x[c] = x[c]+x[d]; x[b] = L32(x[b]^x[c],12);
-  x[a] = x[a]+x[b]; x[d] = L32(x[d]^x[a], 8);
-  x[c] = x[c]+x[d]; x[b] = L32(x[b]^x[c], 7);
-}
-
-static void
-core(unsigned char o[64], const unsigned int in[16])
-{
-  unsigned int x[16];
-  int i;
-
-  for (i = 0; i < 16; ++i) { x[i] = in[i]; }
-  for (i = 0; i < 10; ++i) {
-    /* Column round */
-    QR(x, 0, 4, 8,  12);
-    QR(x, 1, 5, 9,  13);
-    QR(x, 2, 6, 10, 14);
-    QR(x, 3, 7, 11, 15);
-    /* Diagonal round */
-    QR(x, 0, 5, 10, 15);
-    QR(x, 1, 6, 11, 12);
-    QR(x, 2, 7, 8,  13);
-    QR(x, 3, 4, 9,  14);
-  }
-
-  for (i = 0; i < 16; ++i) { x[i] = x[i] + in[i]; } /* Add input */
-  for (i = 0; i < 16; ++i) { st32(o+(4*i), x[i]); } /* Output */
-}
-
-void
-chacha20_kexp(unsigned int* st,
-                            const unsigned char* n,
-                            const unsigned char* k)
-{
-  /* Constants */
-  st[0]  = 0x61707865;
-  st[1]  = 0x3320646e;
-  st[2]  = 0x79622d32;
-  st[3]  = 0x6b206574;
-  /* 4-11: key bytes */
-  st[4]  = ld32(k);
-  st[5]  = ld32(k+4);
-  st[6]  = ld32(k+8);
-  st[7]  = ld32(k+12);
-  st[8]  = ld32(k+16);
-  st[9]  = ld32(k+20);
-  st[10] = ld32(k+24);
-  st[11] = ld32(k+28);
-  /* Counter + Nonce */
-  st[12] = 0;
-  st[13] = 0;
-  st[14] = ld32(n);
-  st[15] = ld32(n+4);
-}
-
-int
-chacha20_xor(unsigned char* c,
-                           const unsigned char* m,
-                           unsigned long long b,
-                           const unsigned char* n,
-                           const unsigned char* k)
-{
-  unsigned long long i;
-  unsigned int st[16];
-  unsigned char blk[64];
-
-  chacha20_kexp(st, n, k);
-
-  for(;;) {
-    /* Advance state */
-    core(blk, st);
-    st[12] = st[12]+1;
-    if (st[12] == 0) { st[13] = st[13]+1; }
-    /* Fast path */
-    if (b <= 64) {
-      for (i = 0; i < b; ++i) c[i] = m[i] ^ blk[i];
-      return 0;
-    }
-    /* Normal path */
-    for (i = 0; i < 64; ++i) { c[i] = m[i] ^ blk[i]; }
-    b -= 64;
-    m += 64;
-    c += 64;
-  }
-}
-
-int
-chacha20_base(unsigned char* o,
-                       unsigned long long ol,
-                       const unsigned char* n,
-                       const unsigned char* k)
-{
-  unsigned long long i;
-  for (i=0; i < ol; ++i) { o[i] = 0; }
-  return chacha20_xor(o,o,ol,n,k);
-}
 
 uint8_t *chacha20(uint8_t *key, uint8_t *nonce, uint8_t *bytes, uint32_t len)
 {
-  if(chacha20_xor(bytes,bytes,len,nonce,key)) return 0;
+  struct chacha_ctx ctx;
+  if(!len) return bytes;
+
+  chacha_keysetup (&ctx, key, 32 * 8);
+  chacha_ivsetup (&ctx, nonce, NULL);
+
+  chacha_encrypt_bytes (&ctx, bytes, bytes, len);
   return bytes;
 }
