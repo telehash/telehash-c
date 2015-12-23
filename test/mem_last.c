cef3ada7ef3923486e5b4a13d6c2f863295d3cb1
Jeremie Miller <jeremie@jabber.org>
2015-12-23 13:30:15 -0700

    warning cleanups

--- test/mem_last.c
+++ test/mem_last.c
@@ -8,7 +8,6 @@ int main(int argc, char **argv)
   printf("%lu\tlink_t\n",sizeof(struct link_struct));
   printf("%lu\tlob_t\n",sizeof(struct lob_struct));
   printf("%lu\tutil_chunk_t\n",sizeof(struct util_chunk_struct));
-  printf("%lu\thashname_t\n",sizeof(struct hashname_struct));
   printf("%lu\te3x_exchange_t\n",sizeof(struct e3x_exchange_struct));
   printf("%lu\tchan_t\n",sizeof(struct chan_struct));
   printf("%lu\tpipe_t\n",sizeof(struct pipe_struct));
